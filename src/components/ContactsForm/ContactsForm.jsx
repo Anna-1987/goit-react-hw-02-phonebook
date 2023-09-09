1f26b77e8a03f95d17a4853851935b9c5dec9004
Anna-1987 <mag260910@gmail.com>
2023-09-09 18:07:38 +0300

    home-1

--- src/components/ContactsForm/ContactsForm.jsx
+++ src/components/ContactsForm/ContactsForm.jsx
@@ -37,7 +37,7 @@ export class ContactsForm extends Component {
                name='name'
                value={name}
                className={css.input}
-               onChange={this. hendlChange}
+               onChange={this.hendlChange}
                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                required
